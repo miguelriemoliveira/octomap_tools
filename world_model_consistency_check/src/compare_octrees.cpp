08d71960da8fe4cd999eb24194e848c8fd752946
rarrais <rafael.arrais@gmail.com>
2015-04-21 18:21:52 +0100

    Import Target and Model to compare_octrees.cpp working

--- world_model_consistency_check/src/compare_octrees.cpp
+++ world_model_consistency_check/src/compare_octrees.cpp
@@ -27,28 +27,46 @@ boost::shared_ptr<ros::Publisher> marker_pub;
 OcTree* octree_model;
 OcTree* octree_target;
 
+std::string topic_model = "/octomap_full";
+std::string topic_target = "/output";
 
-void octomapCallback(const octomap_msgs::Octomap::ConstPtr& msg)
+
+void octomapCallbackModel(const octomap_msgs::Octomap::ConstPtr& msg)
 {
 
     AbstractOcTree* tree = msgToMap(*msg);
     OcTree* octree_model = dynamic_cast<OcTree*>(tree);
 
-    ROS_INFO("\n\nReceived new octree model with id: %s, frame_id: %s\n\n.", msg->id.c_str(), msg->header.frame_id.c_str());
+    ROS_INFO("\n\nReceived new octree model on topic %s with id: %s, frame_id: %s\n\n.", topic_model.c_str(), msg->id.c_str(), msg->header.frame_id.c_str());
+
+}
+
+void octomapCallbackTarget(const octomap_msgs::Octomap::ConstPtr& msg)
+{
+
+    AbstractOcTree* tree = msgToMap(*msg);
+    OcTree* octree_target = dynamic_cast<OcTree*>(tree);
+
+    ROS_INFO("\n\nReceived new octree target on topic %s with id: %s, frame_id: %s\n\n.", topic_target.c_str(), msg->id.c_str(), msg->header.frame_id.c_str());
 
 }
 
 int main (int argc, char** argv)
 {
-    ros::init(argc, argv, "calibrate_reference_system_in_model_node");
+    ros::init(argc, argv, "compare_octrees");
     ros::NodeHandle nh;
+
+    // Use: _topic_model:=/topic_model  and  _topic_target:=/topic_target
+    ros::param::get("~topic_model", topic_model);
+    ros::param::get("~topic_target", topic_target);
 
     //Problem linking? check http://answers.ros.org/question/196935/roslib-reference-error/
     //string path = ros::package::getPath("amazon_object_segmentation");
 
     ros::Duration(1).sleep(); // sleep for half a second
 
-    ros::Subscriber sub = nh.subscribe("/octomap_full", 1, octomapCallback);
+    ros::Subscriber sub_model = nh.subscribe(topic_model, 1, octomapCallbackModel);
+    ros::Subscriber sub_target = nh.subscribe(topic_target, 1, octomapCallbackTarget);
 
     marker_pub = (boost::shared_ptr<ros::Publisher>) (new ros::Publisher);
     *marker_pub = nh.advertise<visualization_msgs::MarkerArray>("/marker_array", 10);
