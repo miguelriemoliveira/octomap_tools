c6cdff6139cab28f8f0f1c24fdccd0d66055eb14
Miguel Riem de Oliveira <m.riem.oliveira@gmail.com>
2015-06-16 15:11:32 +0100

    point cloud accumulator and pcd publisher

--- world_model_consistency_check/src/accumulatepointcloud2file.cpp
+++ world_model_consistency_check/src/accumulatepointcloud2file.cpp
@@ -6,38 +6,36 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/voxel_grid.h>
 
+#include <tf/transform_listener.h>
 #include <sensor_msgs/PointCloud2.h>
+
+#include <pcl_ros/transforms.h>
 
 typedef pcl::PointXYZRGBA PointT;
 
 std::string filename = "output.pcd";
 
 pcl::PointCloud<PointT>::Ptr accumulated_cloud;
-//pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+pcl::VoxelGrid<PointT> grid;
+
+tf::TransformListener *p_listener;
 
 void cloud_open_target (const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-   // sensor_msgs::PointCloud2 msg_filtered;
-   // sor.setInputCloud (*msg);
-   // sor.filter (*msg_filtered);
-
-    
-    
-
     pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
     pcl::PointCloud<PointT>::Ptr cloud_filtered(new pcl::PointCloud<PointT>);
+    pcl::PointCloud<PointT>::Ptr cloud_transformed(new pcl::PointCloud<PointT>);
     pcl::fromROSMsg (*msg, *cloud);
 
-    pcl::VoxelGrid<PointT> grid;
-    //pcl::ApproximateVoxelGrid<PointType> grid;
-    grid.setLeafSize (0.01f, 0.01f, 0.01f);
     grid.setInputCloud (cloud);
     grid.filter (*cloud_filtered);
 
+    pcl_ros::transformPointCloud("/map", *cloud_filtered, *cloud_transformed, *p_listener);
+    //cloud_transformed->header.frame_id = "/map";
 
-
-    (*accumulated_cloud) += (*cloud);
-    //ROS_INFO("Size of accumulated_cloud = %ld", accumulated_cloud->points.size());
+    (*accumulated_cloud) += (*cloud_transformed);
+    accumulated_cloud->header.frame_id;
+    ROS_INFO("Size of accumulated_cloud = %ld", accumulated_cloud->points.size());
 }
 
 
@@ -45,7 +43,7 @@ int main (int argc, char** argv)
 {
 
     // Initialize ROS
-    ros::init (argc, argv, "ros_change_detection");
+    ros::init (argc, argv, "accumulatepointcloud2file");
 
     // Node Handle
     ros::NodeHandle nh;
@@ -54,8 +52,10 @@ int main (int argc, char** argv)
 
 
     accumulated_cloud = (pcl::PointCloud<PointT>::Ptr) new pcl::PointCloud<PointT>;
+    p_listener = (tf::TransformListener*) new tf::TransformListener;
+    
+    grid.setLeafSize (0.03f, 0.03f, 0.03f);
 
-    //sor.setLeafSize (0.01f, 0.01f, 0.01f);
 
     // Create a ROS subscriber for the input point cloud
     ros::Subscriber sub_target = nh.subscribe ("input", 1, cloud_open_target);
---
+++ world_model_consistency_check/src/pcd2pointcloud.cpp
@@ -0,0 +1,71 @@
+//#include <iostream>
+//#include <boost/thread/thread.hpp>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <ros/package.h>
+//#include <rospack/rospack.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <pcl/point_types.h>
+#include <pcl_ros/point_cloud.h>
+
+
+using namespace std;
+using namespace sensor_msgs;
+typedef pcl::PointXYZRGB PointT;
+pcl::PointCloud<PointT>::Ptr cloud(new pcl::PointCloud<PointT>);
+
+int main (int argc, char** argv)
+{
+    ros::init(argc, argv, "pcd2pointcloud", ros::init_options::AnonymousName);
+    ros::NodeHandle nh;
+
+
+    cloud = (pcl::PointCloud<PointT>::Ptr ) new pcl::PointCloud<PointT>;
+
+    string input; 
+    if (ros::param::get("~input", input))
+    {
+        ROS_INFO("Loading point cloud from file %s",input.c_str());
+        if (pcl::io::loadPCDFile<PointT>(input.c_str(), *cloud) != 0)
+        {
+            return -1;
+        }
+        cout << "Cloud has " << cloud->points.size () << " data points!" << endl;
+    }
+    else
+    {
+        ROS_ERROR("No point cloud filename given as input. use _input:=<filename>");
+        exit(1);
+    }
+
+    sensor_msgs::PointCloud2 msg;
+    pcl::toROSMsg (*cloud, msg);
+    msg.header.frame_id = ros::names::remap("/map");
+
+    string output="/output"; 
+    if (ros::param::get("~output", output))
+    {
+        ROS_INFO("Publishing point cloud on topic %s", output.c_str());
+    }
+    else
+    {
+        ROS_WARN("No topic name given, using default %s",output.c_str());
+    }
+
+    ros::Publisher pub = nh.advertise<sensor_msgs::PointCloud2>(output, 1);
+
+
+    ros::Rate loop_rate(1);
+
+    while (ros::ok())
+    {
+
+        pub.publish(msg);
+        loop_rate.sleep();
+        ros::spinOnce();
+    }
+
+    return (0);
+}
+
